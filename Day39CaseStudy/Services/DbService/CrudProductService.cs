8775b83785a6973a5aad57469fde7c16f8eb9d9f
Avinash Tauro <avinash.tauro@arctechinfo.com>
2022-05-16 18:00:30 +0530

    updated code for LINQ bug

--- Day39CaseStudy/Services/DbService/CrudProductService.cs
+++ Day39CaseStudy/Services/DbService/CrudProductService.cs
@@ -24,36 +24,84 @@ public class CrudProductService : ICrudService<Product>
             .Include("Brand")
             .Include("Category")
             .OrderBy(p => p.BrandId)
-                .ThenBy(p=> p.ProductId)
+                .ThenBy(p => p.ProductId)
             .ToList();
 
+        // p1 => this will generate SQL as follows
         /*
-        Select * from production.products
+        Select products.*, brands.*, categories.* from production.products
         inner join production.brands on products.brand_id = brands.brand_id
         inner join production.categories on products.category_id = categories.category_id
         order by
             products.brand_id, products.product_id
         */
 
-        //var p2 = 
-        //    (from product in context.Products
-        //    join brand in context.Brands on product.BrandId equals brand.BrandId
-        //    join category in context.Categories on product.CategoryId equals category.CategoryId
-        //    orderby product.BrandId, product.ProductId
-        //    select product).ToList();
+        var p2 =
+            (from product in context.Products
+             join brand in context.Brands on product.BrandId equals brand.BrandId
+             join category in context.Categories on product.CategoryId equals category.CategoryId
+             orderby product.BrandId, product.ProductId
+             select product).ToList();
 
-        var getProducts = from p2 in context.Products
-            join b in context.Brands
-                on p2.BrandId equals b.BrandId
-            join cat in context.Categories
-                on p2.CategoryId equals cat.CategoryId
-            orderby
-                p2.BrandId, p2.ProductId
-            select p2;
+        // p2 => this will generate SQL as follows
+        //       since only product is select, product.brand and product.category will remain null
+        /*
+        Select products.* from production.products
+        inner join production.brands on products.brand_id = brands.brand_id
+        inner join production.categories on products.category_id = categories.category_id
+        order by
+            products.brand_id, products.product_id
+        */
 
-        var p = getProducts.ToList();
 
-        return p;
+        var p3 =
+            (from product in context.Products
+             join brand in context.Brands on product.BrandId equals brand.BrandId
+             join category in context.Categories on product.CategoryId equals category.CategoryId
+             orderby product.BrandId, product.ProductId
+             select new Product
+             {
+                 ProductId = product.ProductId,
+                 ProductName = product.ProductName,
+                 BrandId = product.BrandId,
+                 CategoryId = product.CategoryId,
+                 ModelYear = product.ModelYear,
+                 ListPrice = product.ListPrice,
+                 Brand = brand,
+                 Category = category
+             }).ToList();
+
+        // p3 => this will generate SQL as follows
+        /*
+        Select 
+            product.ProductId, product.ProductName, product.BrandId, product.CategoryId, product.ModelYear, product.ListPrice,
+            brand.*, category.*
+        from production.products
+        inner join production.brands on products.brand_id = brands.brand_id
+        inner join production.categories on products.category_id = categories.category_id
+        order by
+            products.brand_id, products.product_id
+        */
+
+        // do not use tolist in between
+        var p4 =
+            (from product in context.Products.ToList()
+             join brand in context.Brands on product.BrandId equals brand.BrandId
+             join category in context.Categories on product.CategoryId equals category.CategoryId
+             where product.ProductName.Contains("PO")
+             orderby product.BrandId, product.ProductId
+             select product);
+        // p4 => this will generate SQL as follows
+        /*
+            -- Select * from Products
+            -- select * from Brands
+            -- select * from Category
+
+            -- Load all in memory, then join the enumerables and apply filter. 
+            -- Problem is that all 50000 products (assume) would be loaded in memory 
+            -- resulting in out of memory exception and server crash
+         */
+        return p2;
     }
 
     public void Update(Product product)
